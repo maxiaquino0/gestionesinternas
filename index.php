63686c8610415e49ec601610d199479eb191adac
maxiaquino0 <maxiaquino0@gmail.com>
2014-04-04 23:23:08 -0300

    modificacion con css

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Pantalla de inicio</title>
-	<link rel="StyleSheet" href="style/style.css" type="text/css">
+<meta charset="UTF-8">
+<title>Pantalla de inicio</title>
+<link rel="StyleSheet" href="style/style.css" type="text/css">
 </head>
 <body>
-	<h1 class="tituloprim">Herramienta de Gestiones Internas</h1>
-	<br>
-	<br>
-	<br>
+	<div class="cabecera">
+		<label class="tituloprim">Herramienta de Gestiones Internas</label>
+	</div>
+	<br><br>
+	<hr size="3">
+	<div class="menuweb">
+		<label for="">Bienvenido: Usuario </label>
+		<label for="">  &#124   </label>
+		<a href="">Cerrar Sesion</a>
+	</div>
+	<hr size="3">
+	<div class="subtitulo">
 	<label class="titulosec">Seleccione gestion a Realizar</label>
-	<br>
+	</div>
 	<br>
 	<div id="menu">
 	<p><a href='menu_urg.php'>Urgencias</a></p>
 	<br>
 	<p><a href='menu_pnr.php'>PNR</a></p>
 	</div>
-	<div>
+	<div class="firma">
 		<img src="img/atento_logo.jpg" alt="Atento Logo" width="480" height="170">
 	<br>
 	<br>
